35f29cce91703666f8ffdc818dd811d5a418f1e2
Rhaera <rauldantas@usp.br>
2023-07-15 17:47:38 -0300

    Ajustes Finos nos Métodos Criados e no Ambiente de Testes

---
+++ primeiro-desafio/app/src/test/java/primeiro/desafio/FirstChallengeTest.java
@@ -0,0 +1,10 @@
+package primeiro.desafio;
+
+import org.junit.jupiter.api.Test;
+
+class FirstChallengeTest {
+    @Test void appHasAGreeting() {
+        FirstChallenge classUnderTest = new FirstChallenge();
+        // assertNotNull();
+    }
+}
