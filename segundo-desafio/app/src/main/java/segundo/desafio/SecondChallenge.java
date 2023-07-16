0cc1d2a79dc9a11d806a01b53ec749bc3314e4bc
Rhaera <rauldantas@usp.br>
2023-07-16 06:47:01 -0300

    Implementação da Metodologia Requerida na Main Class
    - "numberOfTimesIntCanBeDividedByOther" (linha 61): Realiza a Obtenção da Parte Inteira do Quociente da Divisão Entre o Valor Selecionado e a Quantia da Cédula/Moeda
    - "moneyPerValueOfNoteAndCoinSelection" (linha 47): Realiza as Iterações Necessárias para Catalogar as Respectivas Quantidades de Cédulas/Moedas que Compõem os Mínimos Valores de Notas/Moedas
    - "moneyPerValueOfNoteAndCoinSelector" (linha 41): Retorna o Modelo Final da Tabela Contendo os Valores Catalogados para Notas e Moedas Necessárias Minimamente para Compor a Quantia Avaliada

--- segundo-desafio/app/src/main/java/segundo/desafio/SecondChallenge.java
+++ segundo-desafio/app/src/main/java/segundo/desafio/SecondChallenge.java
@@ -1,6 +1,8 @@
 package segundo.desafio;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -11,20 +13,20 @@ public class SecondChallenge {
 
     public SecondChallenge() {
         moneyPerValueOfNoteAndCoinMap.putAll(Map.of(
-                BigDecimal.valueOf(100L, 2), 0,
-                BigDecimal.valueOf(50L, 2), 0,
-                BigDecimal.valueOf(20L, 2), 0,
-                BigDecimal.valueOf(10L, 2), 0,
-                BigDecimal.valueOf(5L, 2), 0,
-                BigDecimal.valueOf(2L, 2), 0)
+                BigDecimal.valueOf(10000L, 2), 0,
+                BigDecimal.valueOf(5000L, 2), 0,
+                BigDecimal.valueOf(2000L, 2), 0,
+                BigDecimal.valueOf(1000L, 2), 0,
+                BigDecimal.valueOf(500L, 2), 0,
+                BigDecimal.valueOf(200L, 2), 0)
         );
         moneyPerValueOfNoteAndCoinMap.putAll(Map.of(
-                BigDecimal.valueOf(1L, 2), 0,
-                BigDecimal.valueOf(1L / 2, 2), 0,
-                BigDecimal.valueOf(1L / 4, 2), 0,
-                BigDecimal.valueOf(1L / 10, 2), 0,
-                BigDecimal.valueOf(1L / 20, 2), 0,
-                BigDecimal.valueOf(1L / 100, 2), 0)
+                BigDecimal.valueOf(100L, 2), 0,
+                BigDecimal.valueOf(100L / 2, 2), 0,
+                BigDecimal.valueOf(100L / 4, 2), 0,
+                BigDecimal.valueOf(100L / 10, 2), 0,
+                BigDecimal.valueOf(100L / 20, 2), 0,
+                BigDecimal.valueOf(100L / 100, 2), 0)
         );
     }
 
@@ -32,5 +34,32 @@ public class SecondChallenge {
         SecondChallenge secondChallengeInstance = new SecondChallenge();
         Scanner userInputScanner = new Scanner(System.in);
         System.out.println();
+        long testRhR = userInputScanner.nextLong();
+        System.out.println(secondChallengeInstance.moneyPerValueOfNoteAndCoinSelector(BigDecimal.valueOf(testRhR)));
+    }
+
+    public Map<BigDecimal, Integer> moneyPerValueOfNoteAndCoinSelector(BigDecimal decimalValueSelected) {
+        return decimalValueSelected.compareTo(BigDecimal.ZERO) == 0 ?
+            this.moneyPerValueOfNoteAndCoinMap :
+            moneyPerValueOfNoteAndCoinSelection(decimalValueSelected, new HashMap<>(moneyPerValueOfNoteAndCoinMap));
+    }
+
+    private Map<BigDecimal, Integer> moneyPerValueOfNoteAndCoinSelection(BigDecimal decimalValueSelected, Map<BigDecimal, Integer> copyOfClassMap) {
+        BigDecimal copyOfSelectedValue = decimalValueSelected;
+        for (BigDecimal moneyValueType:
+                copyOfClassMap.keySet()
+                        .stream()
+                        .sorted(Collections.reverseOrder())
+                        .toList()) {
+            int integerPartOfDividedResult = numberOfTimesIntCanBeDividedByOther(copyOfSelectedValue, moneyValueType);
+            copyOfClassMap.put(moneyValueType, integerPartOfDividedResult);
+            copyOfSelectedValue = copyOfSelectedValue.subtract(moneyValueType.multiply(BigDecimal.valueOf(integerPartOfDividedResult)));
+        }
+        return copyOfClassMap;
+    }
+
+    private int numberOfTimesIntCanBeDividedByOther(BigDecimal dividedNumber, BigDecimal divisorNumber) {
+        return dividedNumber.divide(divisorNumber, 0, RoundingMode.DOWN)
+                .intValue();
     }
 }
