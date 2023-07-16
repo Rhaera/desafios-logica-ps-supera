a8ecce0b8835abb37abdb07ce1120d21d85d9a22
Rhaera <rauldantas@usp.br>
2023-07-16 16:56:54 -0300

    Finalizando o Desafio 2 com a Implementação dos Testes Unitários
    - "assertEquals" (linha 16): Para Validar a Igualdade Entre a Classe "HashMap" e a Classe de Retorno do Método Seletor das Notas/Moedas Minimamente Necessárias
    - "assertFalse" (linha 17): Para Validar que, Caso o Valor Monetário Selecionado Seja Minimamente Maior que Zero (Respeitando a Precisão Requerida de Até Duas Casas Decimais), Pelo Menos Uma Cédula e/ou Moeda Será Necessária Para Compor a Quantia Escolhida Pelo Usuário
    - "assertInstanceOf" (linha 22): Para Validar o Comportamento de Instância Única, Conhecido Como "Singleton", Inserido na Main Class, Assegurando que a Classe Principal Somente Pode Ser Instanciada Através do Método "getSingleton"

--- segundo-desafio/app/src/test/java/segundo/desafio/SecondChallengeTest.java
+++ segundo-desafio/app/src/test/java/segundo/desafio/SecondChallengeTest.java
@@ -2,8 +2,23 @@ package segundo.desafio;
 
 import org.junit.jupiter.api.Test;
 
-class SecondChallengeTest {
-    @Test void checkingIfMoneyPerValueOfNoteAndCoinSelectorIsCorrectlyImplemented() {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Random;
+
+class SecondChallengeTest {
+
+    @Test void checkingIfMoneyPerValueOfBillAndCoinSelectorIsCorrectlyImplemented() {
+        assertEquals(HashMap.class, SecondChallenge.getSingleton().moneyPerValueOfBillAndCoinSelector(BigDecimal.valueOf(new Random().nextDouble(0d, 1e6 + 1))).getClass());
+        assertFalse(SecondChallenge.getSingleton()
+                .moneyPerValueOfBillAndCoinSelector(BigDecimal.valueOf(new Random().nextDouble(1e-2, 1e6 + 1)))
+                .values()
+                .stream()
+                .allMatch(probablePositiveDouble -> probablePositiveDouble == 0d));
+        assertInstanceOf(SecondChallenge.class, SecondChallenge.getSingleton());
     }
 }
