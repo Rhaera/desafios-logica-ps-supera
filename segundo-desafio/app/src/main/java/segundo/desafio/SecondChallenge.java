1eee4d1bbf2ac2e9619872c2381864bf0429c698
Rhaera <rauldantas@usp.br>
2023-07-16 13:32:57 -0300

    Implementando a Main Func e Preparando para a Fase de Testes Unitários

--- segundo-desafio/app/src/main/java/segundo/desafio/SecondChallenge.java
+++ segundo-desafio/app/src/main/java/segundo/desafio/SecondChallenge.java
@@ -6,13 +6,18 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class SecondChallenge {
 
-    private final Map<BigDecimal, Integer> moneyPerValueOfNoteAndCoinMap = new HashMap<>();
+    private static final Map<BigDecimal, Integer> moneyPerValueOfBillAndCoinMap = new HashMap<>();
+    private static final SecondChallenge singletonInstance = new SecondChallenge();
+    private static final String NOT_PARSABLE_STRING = "intentionalNotNumericString";
+    private static final String BILLS_STRING = "nota";
+    private static final String COINS_STRING = "moeda";
 
-    public SecondChallenge() {
-        moneyPerValueOfNoteAndCoinMap.putAll(Map.of(
+    private SecondChallenge() {
+        moneyPerValueOfBillAndCoinMap.putAll(Map.of(
                 BigDecimal.valueOf(10000L, 2), 0,
                 BigDecimal.valueOf(5000L, 2), 0,
                 BigDecimal.valueOf(2000L, 2), 0,
@@ -20,7 +25,7 @@ public class SecondChallenge {
                 BigDecimal.valueOf(500L, 2), 0,
                 BigDecimal.valueOf(200L, 2), 0)
         );
-        moneyPerValueOfNoteAndCoinMap.putAll(Map.of(
+        moneyPerValueOfBillAndCoinMap.putAll(Map.of(
                 BigDecimal.valueOf(100L, 2), 0,
                 BigDecimal.valueOf(100L / 2, 2), 0,
                 BigDecimal.valueOf(100L / 4, 2), 0,
@@ -31,20 +36,90 @@ public class SecondChallenge {
     }
 
     public static void main(String[] args) {
-        SecondChallenge secondChallengeInstance = new SecondChallenge();
+        double selectedAmountOfMoney = -1d;
         Scanner userInputScanner = new Scanner(System.in);
         System.out.println();
-        long testRhR = userInputScanner.nextLong();
-        System.out.println(secondChallengeInstance.moneyPerValueOfNoteAndCoinSelector(BigDecimal.valueOf(testRhR)));
+        System.out.println("Olá, bem vindo, vamos começar o desafio!");
+        System.out.println("Para começar, por favor, escolha um valor monetário variando de R$ 0.00 até no máximo R$ 1000000.00:");
+        while (true) {
+            if (selectedAmountOfMoney >= 0 && selectedAmountOfMoney <= 1e6)
+                break;
+            System.out.println();
+            String userInput = userInputScanner.nextLine();
+            System.out.println();
+            if (userInput.trim().equalsIgnoreCase("s"))
+                break;
+            boolean isValidMonetaryValue = singletonInstance.isMonetaryValue(userInput);
+            if (selectedAmountOfMoney < 0 && isValidMonetaryValue)
+                selectedAmountOfMoney = Double.parseDouble(userInput);
+            if (isValidMonetaryValue &&
+                (selectedAmountOfMoney > 1e6 || selectedAmountOfMoney < 0) &&
+                !singletonInstance.isMonetaryValue(NOT_PARSABLE_STRING))
+                selectedAmountOfMoney = -1d;
+        }
+        if (selectedAmountOfMoney >= 0) {
+            System.out.println("Pronto, segue a sequência das notas e das moedas (em ordem decrescente) minimamente necessárias para compor o valor monetário selecionado:");
+            BigDecimal moneyAmountConvertedToBigDecimal = BigDecimal.valueOf(selectedAmountOfMoney * 100).divide(BigDecimal.valueOf(100L), 2, RoundingMode.DOWN);
+            moneyPerValueOfBillAndCoinPrinter(
+                    singletonInstance.moneyPerValueOfBillAndCoinSelector(
+                        moneyAmountConvertedToBigDecimal
+                    ),
+                    BILLS_STRING,
+                    (BigDecimal moneyType) -> moneyType.compareTo(BigDecimal.ONE) > 0
+            );
+            moneyPerValueOfBillAndCoinPrinter(
+                    singletonInstance.moneyPerValueOfBillAndCoinSelector(
+                        moneyAmountConvertedToBigDecimal
+                    ),
+                    COINS_STRING,
+                    (BigDecimal moneyType) -> moneyType.compareTo(BigDecimal.ONE) <= 0
+            );
+        }
+        System.out.println("Obrigado por participar do desafio #2! Volte sempre.");
+        System.out.println();
+        userInputScanner.close();
     }
 
-    public Map<BigDecimal, Integer> moneyPerValueOfNoteAndCoinSelector(BigDecimal decimalValueSelected) {
+    public static SecondChallenge getSingleton() {
+        return singletonInstance;
+    }
+
+    private static void moneyPerValueOfBillAndCoinPrinter(Map<BigDecimal, Integer> monetaryValueCatalogedTable, String toUpperCaseString, Predicate<BigDecimal> moneyMapFilterCriteria) {
+        System.out.println();
+        System.out.println(toUpperCaseString.toUpperCase().concat("S:"));
+        monetaryValueCatalogedTable.keySet()
+            .stream()
+            .sorted()
+            .filter(moneyMapFilterCriteria)
+            .forEach(moneyType ->
+                System.out.println(monetaryValueCatalogedTable.get(moneyType) +
+                    "     ".concat(toUpperCaseString.concat("(s)")) +
+                    "     ".concat("de     R$") +
+                    "     ".concat(moneyType.toString().length() > 5 ? "" + moneyType : (moneyType.toString().length() == 5 ? " " + moneyType : "  " + moneyType))
+                )
+            );
+        System.out.println();
+    }
+
+    private boolean isMonetaryValue(String maybeDouble) {
+        try {
+            Double.parseDouble(maybeDouble);
+            return true;
+        } catch (NumberFormatException failedConversionToDouble) {
+            System.out.println();
+            System.out.println("Desculpe, valor inválido!");
+            System.out.printf("Por favor, digite um valor apropriado para prosseguirmos ou digite %s para sair. ", "'s'");
+            return false;
+        }
+    }
+
+    public Map<BigDecimal, Integer> moneyPerValueOfBillAndCoinSelector(BigDecimal decimalValueSelected) {
         return decimalValueSelected.compareTo(BigDecimal.ZERO) == 0 ?
-            this.moneyPerValueOfNoteAndCoinMap :
-            moneyPerValueOfNoteAndCoinSelection(decimalValueSelected, new HashMap<>(moneyPerValueOfNoteAndCoinMap));
+            moneyPerValueOfBillAndCoinMap :
+            moneyPerValueOfBillAndCoinSelection(decimalValueSelected, new HashMap<>(moneyPerValueOfBillAndCoinMap));
     }
 
-    private Map<BigDecimal, Integer> moneyPerValueOfNoteAndCoinSelection(BigDecimal decimalValueSelected, Map<BigDecimal, Integer> copyOfClassMap) {
+    private Map<BigDecimal, Integer> moneyPerValueOfBillAndCoinSelection(BigDecimal decimalValueSelected, Map<BigDecimal, Integer> copyOfClassMap) {
         BigDecimal copyOfSelectedValue = decimalValueSelected;
         for (BigDecimal moneyValueType:
                 copyOfClassMap.keySet()
@@ -59,7 +134,6 @@ public class SecondChallenge {
     }
 
     private int numberOfTimesIntCanBeDividedByOther(BigDecimal dividedNumber, BigDecimal divisorNumber) {
-        return dividedNumber.divide(divisorNumber, 0, RoundingMode.DOWN)
-                .intValue();
+        return dividedNumber.divide(divisorNumber, 0, RoundingMode.DOWN).intValue();
     }
 }
