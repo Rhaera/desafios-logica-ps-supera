1b946a3ea331d22249cb10e320149ae80aab2b79
Rhaera <rauldantas@usp.br>
2023-07-17 09:22:49 -0300

    Iniciando o Terceiro Desafio

---
+++ terceiro-desafio/app/src/test/java/terceiro/desafio/ThirdChallengeTest.java
@@ -0,0 +1,11 @@
+package terceiro.desafio;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ThirdChallengeTest {
+
+    @Test void appHasAGreeting() {
+        assertNotNull();
+    }
+}
