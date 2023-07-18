03b37daca22e48e64d7f2ad886d380f1d63b78b5
Rhaera <rauldantas@usp.br>
2023-07-18 02:40:09 -0300

    Testes Unitários Realizados

--- quarto-desafio/app/src/test/java/quarto/desafio/FourthChallengeTest.java
+++ quarto-desafio/app/src/test/java/quarto/desafio/FourthChallengeTest.java
@@ -1,11 +1,14 @@
 package quarto.desafio;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class FourthChallengeTest {
 
-    @Test void checkingIf_PUBLIC_METHOD_IsCorrectlyImplemented() {
-        
+    @Test
+    void checkingIfReverseEncryptorIsCorrectlyImplemented() {
+
+        Assertions.assertEquals("SUPERA", FourthChallenge.reverseEncryptor("arepus"));
+
     }
 }
